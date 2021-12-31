44da36fce87a392d0b4d4496910e4e641e68133b
Austin Vecchio <au.vecchio@gmail.com>
2021-12-31 15:33:16 -0700

    Copying code over from linode

--- do_volume.go
+++ do_volume.go
@@ -1,100 +1,112 @@
 package catacomb
 
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// Additional imports needed for examples below
+
 type DoSpaceConfig struct {
 }
 
 type DoSpaceSession struct {
+	bucket string
+	client *s3.S3
 }
 
-func doSpaceNewSession(config LinodeVolumeConfig) LinodeVolumeSession {
+func DoSpaceNewSession(config DoSpaceConfig) DoSpaceSession {
+	key := os.Getenv("SPACES_KEY")
+	secret := os.Getenv("SPACES_SECRET")
+	s3Config := &aws.Config{
+		Credentials: credentials.NewStaticCredentials(key, secret, ""),
+		Endpoint:    aws.String("https://nyc3.digitaloceanspaces.com"),
+		Region:      aws.String("us-east-1"),
+	}
 
-	/*
-		key := os.Getenv("SPACES_KEY")
-			secret := os.Getenv("SPACES_SECRET")
+	newSession := session.New(s3Config)
+	s3Client := s3.New(newSession)
 
-			s3Config := &aws.Config{
-				Credentials: credentials.NewStaticCredentials(key, secret, ""),
-				Endpoint:    aws.String("https://nyc3.digitaloceanspaces.com"),
-				Region:      aws.String("us-east-1"),
-			}
-
-			newSession := session.New(s3Config)
-			s3Client := s3.New(newSession)
-	*/
-
-	session := LinodeVolumeSession{}
+	session := DoSpaceSession{
+		client: s3Client,
+	}
 	return session
 }
 
-func (session LinodeVolumeSession) doSpaceCloseSession() {
+func (session DoSpaceSession) doSpaceCloseSession() {
 
 }
 
 func (session DoSpaceSession) doSpaceListObjects() {
-	/*
-	   input := &s3.ListObjectsInput{
-	     Bucket:  aws.String("example-space-name"),
-	   }
+	input := &s3.ListObjectsInput{
+		Bucket: aws.String("example-space-name"),
+	}
 
-	   objects, err := s3Client.ListObjects(input)
-	   if err != nil {
-	     fmt.Println(err.Error())
-	   }
+	objects, err := session.client.ListObjects(input)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-	   for _, obj := range objects.Contents {
-	     fmt.Println(aws.StringValue(obj.Key))
-	   }
-	*/
+	for _, obj := range objects.Contents {
+		fmt.Println(aws.StringValue(obj.Key))
+	}
 }
 
-func (session DoSpaceSession) doSpaceObjectUpload() {
-	/*object := s3.PutObjectInput{
-		Bucket: aws.String("example-space-name"),
+func (session DoSpaceSession) doSpaceUpload() {
+	object := s3.PutObjectInput{
+		Bucket: aws.String(session.bucket),
 		Key:    aws.String("file.ext"),
 		Body:   strings.NewReader("The contents of the file."),
 		ACL:    aws.String("private"),
 		Metadata: map[string]*string{
-								 "x-amz-meta-my-key": aws.String("your-value"), //required
-						 },
+			"x-amz-meta-my-key": aws.String("your-value"), //required
+		},
 	}
-	_, err := s3Client.PutObject(&object)
+	_, err := session.client.PutObject(&object)
 	if err != nil {
 		fmt.Println(err.Error())
-	}*/
+	}
 }
 
-func (session DoSpaceSession) doSpaceObjectDownload() {
-	/*
-	   input := &s3.GetObjectInput{
-	       Bucket: aws.String("example-space-name"),
-	       Key:    aws.String("file.ext"),
-	   }
+func (session DoSpaceSession) doSpaceDownload(objectKey string) {
 
-	   result, err := s3Client.GetObject(input)
-	   if err != nil {
-	       fmt.Println(err.Error())
-	   }
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(session.bucket),
+		Key:    aws.String(objectKey),
+	}
 
-	   out, err := os.Create("/tmp/local-file.ext")
-	   defer out.Close()
+	result, err := session.client.GetObject(input)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-	   _, err = io.Copy(out, result.Body)
-	   if err != nil {
-	       fmt.Println(err.Error())
-	   }
-	*/
+	out, err := os.Create("/tmp/local-file.ext")
+	defer out.Close()
+
+	_, err = io.Copy(out, result.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
 
-func (session DoSpaceSession) doSpaceObjectDelete() {
-	/*
-	   input := &s3.DeleteObjectInput{
-	       Bucket: aws.String("example-space-name"),
-	       Key:    aws.String("example-file-to-delete.ext"),
-	   }
+func (session DoSpaceSession) doSpaceDelete(objectKey string) {
 
-	   result, err := s3Client.DeleteObject(input)
-	   if err != nil {
-	       fmt.Println(err.Error())
-	   }
-	*/
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(session.bucket),
+		Key:    aws.String(objectKey),
+	}
+
+	_, err := session.client.DeleteObject(input)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
